49a242953b81da8586c9123007facfc6ff789bce
kaibad <badukailash001@gmail.com>
2025-07-08 12:53:52 +0545

    Setup React project using Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-[100vh] ">
+        <h1 className="text-4xl font-bold text-amber-700 ">
+          Welcome to SujhavMitra
+        </h1>
+      </div>
+    </>
+  );
+}
